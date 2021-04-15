b17fc549aa45362824ee2fb99743d62129faf445
D-Card <diogocardoso67@tecnico.ulisboa.pt>
2021-04-15 19:04:06 +0100

    change formatting, reduce functions size

--- kanban.c
+++ kanban.c
@@ -12,17 +12,6 @@
 #include "constants.h"
 #include "prototypes.h"
 
-typedef struct
-{
-    int id;
-    int expectedDuration;
-    int startTime;
-    char description[MAX_DSCPRT_LENGTH];
-    char user[MAX_USER_LENGTH];
-    char activity[MAX_ACTV_LENGTH];
-
-} Task;
-
 /*global variables*/
 char users[MAX_USERS][MAX_USER_LENGTH];                                        /*tracks all users*/
 char activities[MAX_ACTIVITIES][MAX_ACTV_LENGTH] = {TO_DO, IN_PROGRESS, DONE}; /*tracks all activities*/
@@ -34,15 +23,12 @@ int taskOrder[MAX_TASKS];                                                      /
 Task allTasks[MAX_TASKS];                                                      /*tracks all tasks*/
 
 /* reads command letter and runs appropriate function */
-int main()
-{
+int main(){
     int c, run = 1;
 
-    while (run == 1)
-    {
+    while (run == 1){
         c = getchar();
-        switch (c)
-        {
+        switch (c){
         case 'q':
             run = 0;
             break;
@@ -62,7 +48,7 @@ int main()
             MoveTask();
             break;
         case 'd':
-            ListTasksinActivity();
+            ListTasksInActivity();
             break;
         case 'a':
             AddActivity();
@@ -73,13 +59,11 @@ int main()
 }
 
 /* reads 1 argument from input, saves what it reads into string and returns the last character read */
-char SanitizeArgument(char *string)
-{
+char SanitizeArgument(char *string){
     int c, i = 0, flag = 0; /* flags whether a character other than space/tab has been read */
 
     /* copy everything between blank characters */
-    while ((c = getchar()) != '\n' && (c != ' ' || flag == 0))
-    {
+    while ((c = getchar()) != '\n' && (c != ' ' || flag == 0)){
         if (c != ' ' && c != '\t')
             flag = 1;
         if (flag == 1)
@@ -90,13 +74,11 @@ char SanitizeArgument(char *string)
 }
 
 /* reads a string from input and saves what it reads into string */
-void SanitizeString(char *string)
-{
+void SanitizeString(char *string){
     int c, i = 0, flag = 0; /* flags whether a character other than space/tab has been read */
 
     /* copy everything starting at non-space/tab character until end of command*/
-    while ((c = getchar()) != '\n')
-    {
+    while ((c = getchar()) != '\n'){
         if (c != ' ' && c != '\t')
             flag = 1;
         if (flag == 1)
@@ -106,29 +88,14 @@ void SanitizeString(char *string)
 }
 
 /* ran when "t" command is invoked. adds task to program */
-void AddTask()
-{
+void AddTask(){
     Task newTask;
-    int i;
 
     scanf("%d", &newTask.expectedDuration);
     SanitizeString(newTask.description);
-    if (totalTasks == MAX_TASKS)
-    {
-        printf(TASK_LIMIT_ERROR);
+
+    if (CheckAddTaskErrors(newTask) == 1)
         return;
-    }
-    for (i = 0; i < totalTasks; ++i)
-        if (strcmp(newTask.description, allTasks[i].description) == 0)
-        {
-            printf(DESCRIPTION_EXISTS_ERROR);
-            return;
-        }
-    if (newTask.expectedDuration < 1)
-    {
-        printf(DURATION_ERROR);
-        return;
-    }
 
     /* update task struct*/
     allTasks[totalTasks] = newTask;
@@ -142,20 +109,38 @@ void AddTask()
     printf("%s %d\n", TASK, totalTasks);
 }
 
+/* auxiliar function of AddTask(). checks for input errors */
+int CheckAddTaskErrors(Task task){
+    int i;
+
+    if (totalTasks == MAX_TASKS){
+        printf(TASK_LIMIT_ERROR);
+        return 1;
+    }
+    for (i = 0; i < totalTasks; ++i)
+        if (strcmp(task.description, allTasks[i].description) == 0){
+            printf(DESCRIPTION_EXISTS_ERROR);
+            return 1;
+        }
+    if (task.expectedDuration < 1){
+        printf(DURATION_ERROR);
+        return 1;
+    }
+
+    return 0;
+}
+
 /* auxiliar function of AddTask(). performs binary search and inserts newTask into correct place in allTasks vector*/
-void OrderNewTask(char *description)
-{
+void OrderNewTask(char *description){
     int i, first, middle, last, index;
     /* binary search */
     if (totalTasks == 0)
         index = 0;
-    else
-    {
+    else{
         first = 0;
         last = totalTasks - 1;
         middle = (first + last) / 2;
-        while (first < last) /* when first == last, task should either be in that index or 1 higher */
-        {
+        while (first < last){ /* when first == last, task should either be in that index or 1 higher */
             if (strcmp(allTasks[taskOrder[middle] - 1].description, description) < 0)
                 first = middle + 1;
             else
@@ -176,16 +161,13 @@ void OrderNewTask(char *description)
 }
 
 /* ran when "l" command is invoked. lists all tasks or specified tasks */
-void ListTasks()
-{
+void ListTasks(){
     char lastChar, id[MAX_TASKS_STRING_SIZE];
     int flag = 0;
 
-    while (flag == 0)
-    {
+    while (flag == 0){
         lastChar = SanitizeArgument(id);
-        if (id[0] == '\0') /* check if command has no arguments */
-        {
+        if (id[0] == '\0'){ /* check if command has no arguments */
             ListAllTasks();
             return;
         }
@@ -202,10 +184,9 @@ void ListTasks()
     }
 }
 
-/* auxiliar function of ListTasks() */
-void ListAllTasks()
+/* auxiliar function of ListTasks(). lists all tasks */
+void ListAllTasks(){
 /* no need to sort tasks as they're already ordered in taskOrder vector */
-{
     int i;
 
     for (i = 0; i < totalTasks; ++i)
@@ -217,18 +198,15 @@ void ListAllTasks()
 }
 
 /* ran when "n" command is invoked. advances program time */
-void AdvanceTime()
-{
+void AdvanceTime(){
     int timeAdded;
 
     scanf("%d", &timeAdded);
-    if (timeAdded < 0)
-    {
+    if (timeAdded < 0){
         printf(TIME_ERROR);
         return;
     }
-    if (timeAdded == 0)
-    {
+    if (timeAdded == 0){
         printf("%d\n", time);
         return;
     }
@@ -238,27 +216,23 @@ void AdvanceTime()
 }
 
 /* ran when "u" command is invoked. adds user or lists all users */
-void AddUser()
-{
+void AddUser(){
     char user[MAX_USER_LENGTH];
     int i;
 
     SanitizeArgument(user);
 
-    if (user[0] == '\0') /* check if command has no arguments */
-    {
+    if (user[0] == '\0'){ /* check if command has no arguments */
         for (i = 0; i < totalUsers; ++i)
             printf("%s\n", users[i]);
         return;
     }
-    else if (totalUsers == 50)
-    {
+    else if (totalUsers == 50){
         printf(USER_LIMIT_ERROR);
         return;
     }
     for (i = 0; i < totalUsers; ++i)
-        if (strcmp(users[i], user) == 0)
-        {
+        if (strcmp(users[i], user) == 0){
             printf(USER_EXISTS_ERROR);
             return;
         }
@@ -268,48 +242,20 @@ void AddUser()
 }
 
 /* ran when "m" command is invoked. moves task between activities */
-void MoveTask()
-{
-    int id, i, user_exists = 0, activity_exists = 0;
+void MoveTask(){
+    int id;
     char user[MAX_USER_LENGTH], activity[MAX_ACTV_LENGTH];
 
     scanf("%d", &id);
     SanitizeArgument(user);
     SanitizeString(activity);
 
-    if (id > totalTasks || id < 1)
-    {
-        printf(NO_TASK_ERROR);
+    if (CheckMoveTaskErrors(id, user, activity) == 1)
         return;
-    }
-    if (strcmp(activity, TO_DO) == 0)
-    {
-        if (strcmp(allTasks[id - 1].activity, TO_DO) == 0)
-            return; /*if moving from TO DO to TO DO do nothing*/
-        printf(TASK_STARTED_ERROR);
-        return;
-    }
-    for (i = 0; i < totalUsers; ++i)
-        if (strcmp(user, users[i]) == 0)
-            user_exists = 1;
-    if (user_exists == 0)
-    {
-        printf(NO_USER_ERROR);
-        return;
-    }
-    for (i = 0; i < totalActivities; ++i)
-        if (strcmp(activity, activities[i]) == 0)
-            activity_exists = 1;
-    if (activity_exists == 0)
-    {
-        printf(NO_ACTIVITY_ERROR);
-        return;
-    }
 
     if (strcmp(allTasks[id - 1].activity, TO_DO) == 0) /*if task is being moved from TO DO set start time*/
         allTasks[id - 1].startTime = time;
-    if (strcmp(activity, DONE) == 0) /*if task is being moved to DONE print required info*/
-    {
+    if (strcmp(activity, DONE) == 0){ /*if task is being moved to DONE print required info*/
         if (strcmp(allTasks[id - 1].activity, DONE) == 0) /*if being moved from DONE to DONE do nothing*/
             return;
         printf("%s=%d %s=%d\n",
@@ -324,19 +270,49 @@ void MoveTask()
     strcpy(allTasks[id - 1].user, user);
 }
 
+/* auxiliar function of AddActivity(). checks for input errors */
+int CheckMoveTaskErrors(int id, char *user, char *activity){
+    int i, user_exists = 0, activity_exists = 0;
+
+    if (id > totalTasks || id < 1){
+        printf(NO_TASK_ERROR);
+        return 1;
+    }
+    if (strcmp(activity, TO_DO) == 0){
+        if (strcmp(allTasks[id - 1].activity, TO_DO) == 0)
+            return 1; /*if moving from TO DO to TO DO do nothing*/
+
+        printf(TASK_STARTED_ERROR);
+        return 1;
+    }
+    for (i = 0; i < totalUsers; ++i)
+        if (strcmp(user, users[i]) == 0)
+            user_exists = 1;
+    if (user_exists == 0){
+        printf(NO_USER_ERROR);
+        return 1;
+    }
+    for (i = 0; i < totalActivities; ++i)
+        if (strcmp(activity, activities[i]) == 0)
+            activity_exists = 1;
+    if (activity_exists == 0){
+        printf(NO_ACTIVITY_ERROR);
+        return 1;
+    }
+    return 0;
+}
+
 /* ran when "d" command is invoked. lists tasks in specified activity */
-void ListTasksinActivity()
-{
+void ListTasksInActivity(){
     char activity[MAX_ACTV_LENGTH];
-    int i, j, temp, idOrder[MAX_TASKS], count = 0, activity_exists = 0;
+    int i, j, idOrder[MAX_TASKS], count = 0, activity_exists = 0;
 
     SanitizeString(activity);
 
     for (i = 0; i < totalActivities; ++i)
         if (strcmp(activity, activities[i]) == 0)
             activity_exists = 1;
-    if (activity_exists == 0)
-    {
+    if (activity_exists == 0){
         printf(NO_ACTIVITY_ERROR);
         return;
     }
@@ -344,18 +320,28 @@ void ListTasksinActivity()
     j = 0;
     /*copy id of tasks in activity to idOrder vector and count number of tasks*/
     for (i = 0; i < totalTasks; ++i)
-        if (strcmp(allTasks[taskOrder[i] - 1].activity, activity) == 0)
-        {
+        if (strcmp(allTasks[taskOrder[i] - 1].activity, activity) == 0){
             idOrder[j++] = taskOrder[i];
             ++count;
         }
 
+    SortTasksByTime(idOrder, count);
+
+    for (i = 0; i < count; ++i)
+        printf("%d %d %s\n", 
+               idOrder[i],
+               allTasks[idOrder[i] - 1].startTime,
+               allTasks[idOrder[i] - 1].description);
+}
+
+/* auxiliar function of ListTasksInActivity(). sorts tasks by start time */
+void SortTasksByTime(int *idOrder, int count){
+    int i, j, temp;
+
     /*sort tasks by startTime*/
     for (i = 0; i < count; ++i)
-        for (j = i + 1; j < count; ++j)
-        {
-            if (allTasks[idOrder[i] - 1].startTime > allTasks[idOrder[j] - 1].startTime)
-            {
+        for (j = i + 1; j < count; ++j){
+            if (allTasks[idOrder[i] - 1].startTime > allTasks[idOrder[j] - 1].startTime){
                 temp = idOrder[i];
                 idOrder[i] = idOrder[j];
                 idOrder[j] = temp;
@@ -363,58 +349,54 @@ void ListTasksinActivity()
             /*if startTime is the same, sort them alphabetically by description*/
             else if (allTasks[idOrder[i] - 1].startTime == allTasks[idOrder[j] - 1].startTime &&
                      strcmp(allTasks[idOrder[i] - 1].description,
-                            allTasks[idOrder[j] - 1].description) > 0)
-            {
+                            allTasks[idOrder[j] - 1].description) > 0){
                 temp = idOrder[i];
                 idOrder[i] = idOrder[j];
                 idOrder[j] = temp;
             }
         }
-
-    for (i = 0; i < count; ++i)
-        printf("%d %d %s\n",
-               idOrder[i],
-               allTasks[idOrder[i] - 1].startTime,
-               allTasks[idOrder[i] - 1].description);
 }
 
 /* ran when "a" command is invoked. adds activity to program */
-void AddActivity()
-{
+void AddActivity(){
     char activity[MAX_ACTV_LENGTH];
     int i;
 
     SanitizeString(activity);
 
-    if (activity[0] == '\0') /* check if command has no arguments */
-    {
+    if (activity[0] == '\0'){ /* check if command has no arguments */
         for (i = 0; i < totalActivities; ++i)
             printf("%s\n", activities[i]);
         return;
     }
-    if (totalActivities == 10)
-    {
-        printf(ACTIVITY_LIMIT_ERROR);
+    if (CheckActivityErrors(activity) == 1)
         return;
-    }
-    for (i = 0; i < totalActivities; ++i)
-        if (strcmp(activity, activities[i]) == 0)
-        {
-            printf(ACTIVITY_EXISTS_ERROR);
-            return;
-        }
-    /* check if activity has lower-case letters */
-    for (i = 0; i < (int)strlen(activity); ++i)
-    {
-        if (activity[i] == '\0')
-            break;
-        if (islower(activity[i]))
-        {
-            printf(DESCRIPTION_ERROR);
-            return;
-        }
-    }
 
     strcpy(activities[totalActivities], activity);
     ++totalActivities;
+}
+
+/* auxiliar function of AddActivity(). checks for input errors */
+int CheckActivityErrors(char *activity){
+    int i;
+
+    if (totalActivities == MAX_ACTIVITIES){
+        printf(ACTIVITY_LIMIT_ERROR);
+        return 1;
+    }
+    for (i = 0; i < totalActivities; ++i)
+        if (strcmp(activity, activities[i]) == 0){
+            printf(ACTIVITY_EXISTS_ERROR);
+            return 1;
+        }
+    /* check if activity has lower-case letters */
+    for (i = 0; i < (int)strlen(activity); ++i){
+        if (activity[i] == '\0')
+            break;
+        if (islower(activity[i])){
+            printf(DESCRIPTION_ERROR);
+            return 1;
+        }
+    }
+    return 0;
 }
--- prototypes.h
+++ prototypes.h
@@ -1,17 +1,31 @@
 /*
  * File:  prototypes.h
  * Author:  Diogo Cardoso 99209
- * Description: File where all functions' prototypes are declared.
+ * Description: File where all functions' prototypes + Task struct are declared.
 */
+
+typedef struct{
+    int id;
+    int expectedDuration;
+    int startTime;
+    char description[MAX_DSCPRT_LENGTH];
+    char user[MAX_USER_LENGTH];
+    char activity[MAX_ACTV_LENGTH];
+
+} Task;
 
 void SanitizeString(char *string);
 char SanitizeArgument(char *string);
 void AddTask();
+int CheckAddTaskErrors(Task task);
 void OrderNewTask(char *description);
 void ListTasks();
 void ListAllTasks();
 void AdvanceTime();
 void AddUser();
 void MoveTask();
-void ListTasksinActivity();
+int CheckMoveTaskErrors(int id, char *user, char *activity);
+void ListTasksInActivity();
+void SortTasksByTime(int *idOrder, int count);
 void AddActivity();
+int CheckActivityErrors(char *activity);
